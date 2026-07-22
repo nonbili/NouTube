536d27bfadb78af513d64243b1cb1cc531d4de5e
Ping Chen <remotenonsense@gmail.com>
2026-07-22 11:52:56 +0900

    android: 0.6.4

--- scripts/prepare-dev.sh
+++ scripts/prepare-dev.sh
@@ -3,3 +3,5 @@ set -e
 sed -i '' 's/jp.nonbili.noutube/jp.nonbili.noutube_dev/' app.config.ts
 sed -i '' 's/NouTube/NouTube-dev/' app.config.ts
 yes | bun expo prebuild -p android --clean --no-install
+sed -i '' 's/release {/release {\
+            signingConfig signingConfigs.debug/' android/app/build.gradle
